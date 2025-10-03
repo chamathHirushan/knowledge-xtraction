9e3972012b1aac1bfef9e6dea1bf3abd63559841
chamathHirushan <chamathg.21@cse.mrt.ac.lk>
2025-10-03 07:08:37 +0530

    update: mesaqa

--- src/extraction/pubmed.py
+++ src/extraction/pubmed.py
@@ -6,12 +6,12 @@ def load_pubmedqa():
     cleans the 'context' field, and returns a combined DataFrame.
     """
     # Load parquet files from HuggingFace Hub
-    df1 = pd.read_parquet("hf://datasets/qiaojin/PubMedQA/pqa_unlabeled/train-00000-of-00001.parquet")
-    df2 = pd.read_parquet("hf://datasets/qiaojin/PubMedQA/pqa_labeled/train-00000-of-00001.parquet")
-    df3 = pd.read_parquet("hf://datasets/qiaojin/PubMedQA/pqa_artificial/train-00000-of-00001.parquet")
+    df = pd.read_parquet("hf://datasets/riiwang/MESAQA/train/train-00000-of-00001.parquet")
+    # df2 = pd.read_parquet("hf://datasets/qiaojin/PubMedQA/pqa_labeled/train-00000-of-00001.parquet")
+    # df3 = pd.read_parquet("hf://datasets/qiaojin/PubMedQA/pqa_artificial/train-00000-of-00001.parquet")
 
     # Concatenate all datasets
-    df = pd.concat([df1, df2, df3], ignore_index=True)
+    #df = pd.concat([df1, df2, df3], ignore_index=True)
 
     # Extract the first context string from dicts
     df["context_clean"] = df["context"].apply(
